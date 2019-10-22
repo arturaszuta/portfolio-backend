317dbca91718a58a0899ace152c2c39e5e01030f
Arturas Zuta <arturaszuta@gmail.com>
2019-10-22 14:38:18 -0400

    fix post route

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ require('dotenv').config()
 const express = require("express")
 const app = express();
 const nodemailer = require('nodemailer');
+const util = require("util")
+
+var bodyParser = require('body-parser')
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*" ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use( bodyParser.urlencoded({ extended: true }));
 
 async function main(name, email, message) {
 
@@ -25,6 +36,7 @@ async function main(name, email, message) {
 }
 
 app.post("/sendform", (req, res) => {
+ 
   const { name, email, message } = req.query;
   main(name, email, message);
   res.send("Success!");
